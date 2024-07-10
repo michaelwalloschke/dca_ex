7d86ab45560630b9f7562e5c9221636cc51133dd
WALLOSCHKE Michael <michael.walloschke@axians.de>
2024-07-10 10:41:47 +0200

    added ex_doc and coverage

--- apps/smtp/mix.exs
+++ apps/smtp/mix.exs
@@ -11,7 +11,16 @@ defmodule Smtp.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ],
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -28,6 +37,20 @@ defmodule Smtp.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get"],
+      # setup: ["deps.get", "ecto.setup"],
+      # "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      # "ecto.reset": ["ecto.drop", "ecto.setup"],
+      # test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["test"]
     ]
   end
 end
--- mix.exs
+++ mix.exs
@@ -6,7 +6,27 @@ defmodule DcaEx.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        ensure_consistency: :test
+      ],
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: [plt_add_deps: :app_tree],
+      deps: deps(),
+      aliases: aliases(),
+
+      # Docs
+      name: "DcaEx",
+      source_url: "https://github.com/Taraluktus/dca_ex"
+    #   homepage_url: "http://YOUR_PROJECT_HOMEPAGE",
+    # docs: [
+    #   main: "MyApp", # The main page in the docs
+    #   logo: "path/to/logo.png",
+    #   extras: ["README.md"]
+    # ]
     ]
   end
 
@@ -17,9 +37,31 @@ defmodule DcaEx.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:jason, "~> 1.4"},
+
+      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  #
+  # Aliases listed here are available only for this project
+  # and cannot be accessed from applications inside the apps/ folder.
+  defp aliases do
+    [
+      # run `mix setup` in all child apps
+      setup: ["cmd mix setup"],
+      ensure_consistency: ["test", "dialyzer", "credo --strict", "coveralls"]
+    ]
   end
+
+end
