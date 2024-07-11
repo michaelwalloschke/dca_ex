f0c38c7519c49d581e0fec734bbcf9c1dfd6518e
WALLOSCHKE Michael <michael.walloschke@axians.de>
2024-07-11 17:45:49 +0200

    feat: add token field to User struct - Add `token` field to the `User` struct - Update the `@enforce_keys` to only require the `name` field - Add documentation for the new `token` field

--- apps/user/lib/user.ex
+++ apps/user/lib/user.ex
@@ -1,9 +1,19 @@
 defmodule User do
   @moduledoc """
-  Documentation for `User`.
+  General user struct `User`
+
+  Fields:
+  - `name` - The user's name.
+  - `token` - The user's token (used internally).
+  - `hash` - The user's password hash.
+  - `email` - The user's email address.
+  - `disabled` - Whether the user is disabled.
+  - `display_name` - The user's display name.
+  - `created_at` - The user's creation date.
+  - `last_login_at` - The user's last login date.
   """
   @moduledoc since: "0.1.0"
 
-  @enforce_keys [:name]
-  defstruct [:name, :hash, :email, :disabled, :display_name, :created_at, :last_login_at]
+  @enforce_keys :name
+  defstruct [:name, :token, :hash, :email, :disabled, :display_name, :created_at, :last_login_at]
 end
